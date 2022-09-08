9ac89de5318f98351f550cf50ba67729cc3f388f
Alejandro Sanchez <aalejo@gmail.com>
2022-09-08 06:45:50 +0000

    fixed daphner runner by removing websockets

--- breathecode/asgi.py
+++ breathecode/asgi.py
@@ -7,7 +7,6 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
 
-from breathecode.websocket.urls import websocket_urlpatterns
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import breathecode.settings as app_settings
@@ -20,7 +19,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'breathecode.settings')
 
 django.setup()
 app = get_asgi_application()
-
 
 # settings.configure(INSTALLED_APPS=app_settings.INSTALLED_APPS, DATABASES=app_settings.DATABASES)
 
