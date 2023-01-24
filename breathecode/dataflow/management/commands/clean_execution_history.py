1b298814e0ba9c10ccb27caa685c18a6800d4d6f
Charlytoc <charlyjchaconc@gmail.com>
2023-01-24 16:47:11 +0000

    code more readable

--- breathecode/dataflow/management/commands/clean_execution_history.py
+++ breathecode/dataflow/management/commands/clean_execution_history.py
@@ -1,11 +1,5 @@
-import os, requests, sys, pytz, datetime
-from django.utils import timezone
-from django.db.models.expressions import RawSQL
 from django.core.management.base import BaseCommand, CommandError
-from django.db import models as DM
-from django.db.models import Q, F
 from ...models import PipelineExecution
-from ...tasks import async_run_pipeline
 
 
 class Command(BaseCommand):
@@ -13,22 +7,22 @@ class Command(BaseCommand):
         parser.add_argument('pipeline', type=str)
 
     def handle(self, *args, **options):
-        filter_by = sys.argv[2]
-        if filter_by == None:
-            raise Exception('Invalidad pipeline slug')
-        if filter_by == 'all':
+        pipeline_slug = sys.argv[2]
+        if pipeline__slug == None:
+            raise Exception('Missing pipeline slug')
+        if pipeline_slug == 'all':
             executions = PipelineExecution.objects.all()
             if len(executions) == 0:
-                return print('No matching records found in the database.')
+                print('No matching records found in the database.')
             else:
                 executions.delete()
-                return print('Records deleted successfully')
-        if filter_by != None:
-            executions = PipelineExecution.objects.filter(pipeline__slug=filter_by).all()
+                print('Records deleted successfully')
+        else:
+            executions = PipelineExecution.objects.filter(pipeline__slug=pipeline_slug).all()
             if len(executions) == 0:
-                return print('No matching records found in the database.')
+                print('No matching records found in the database.')
             else:
                 executions.delete()
-                return print('Records deleted successfully')
+                print('Records deleted successfully')
         
 
